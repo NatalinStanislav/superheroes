2066a565ff58c2ce3ffdafd521a0b70852ba3184
Natalin Stanislav <stanislav.natalin@citeck.ru>
2021-12-13 17:21:08 +0300

    Lesson 5 after 5 homework

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,6 +4,7 @@ import 'package:superheroes/blocs/main_bloc.dart';
 import 'package:superheroes/pages/superheroes_images.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
 import 'package:superheroes/widgets/action_button.dart';
+import 'package:superheroes/widgets/superhero_card.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
@@ -70,15 +71,16 @@ class MainPageStateWidget extends StatelessWidget {
         final MainPageState state = snapshot.data!;
         switch (state) {
           case MainPageState.loading:
-            return LoadingIndicator();
+          return LoadingIndicator();
           case MainPageState.noFavorites:
-            return NoFavoritesStateWidget();
+          return NoFavoritesStateWidget();
           case MainPageState.minSymbols:
-            return MinSymbolsStateWidget();
+          return MinSymbolsStateWidget();
           case MainPageState.nothingFound:
           case MainPageState.loadingError:
           case MainPageState.searchResults:
           case MainPageState.favorites:
+            return FavoritesStateWidget();
           default:
             return Center(
                 child: Text(
@@ -87,6 +89,58 @@ class MainPageStateWidget extends StatelessWidget {
             ));
         }
       },
+    );
+  }
+}
+
+class FavoritesStateWidget extends StatelessWidget {
+  const FavoritesStateWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 90),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Your favorites",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SuperheroCard(
+              imageUrl:
+                  "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg",
+              name: "Batman",
+              realName: "Bruce Wayne",
+            ),
+          ),
+          SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SuperheroCard(
+              imageUrl:
+                  "https://www.superherodb.com/pictures2/portraits/10/100/85.jpg",
+              name: "Ironman",
+              realName: "Tony Stark",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/superhero_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:superheroes/resources/superheroes_colors.dart';
+
+class SuperheroCard extends StatelessWidget {
+  final String name;
+  final String realName;
+  final String imageUrl;
+
+  const SuperheroCard({
+    Key? key,
+    required this.name,
+    required this.realName,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      color: SuperheroesColors.superheroCardBackground,
+      child: Row(
+        children: [
+          Image.network(
+            imageUrl,
+            width: 70,
+            height: 70,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name.toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  realName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
